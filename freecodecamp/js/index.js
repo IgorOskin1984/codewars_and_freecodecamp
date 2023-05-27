5c8a349f857252aa50619d4f075165b54232b91a
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-27 12:37:32 +0300

    Add Two Numbers with JavaScript

--- freecodecamp/js/index.js
+++ freecodecamp/js/index.js
@@ -1,17 +1,1 @@
-// Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
-
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
-
-let catName = "Oliver";
-let catSound = "Meow!";
-
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+const sum = 10 + 10;
