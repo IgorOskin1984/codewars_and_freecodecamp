186a9d001dc7aa10f18d726096db49ce076de31a
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-27 13:37:34 +0300

    Appending Variables to Strings

--- freecodecamp/js/index.js
+++ freecodecamp/js/index.js
@@ -1,3 +1,4 @@
-// Only change code below this line
-const myName = "Ihor";
-const myStr = "My name is " + myName + ' and I am well!';
+// Change code below this line
+const someAdjective = "cool";
+let myStr = "Learning to code is ";
+myStr += someAdjective
