6dde61d0ba0d337a43133a93241553c613d8a947
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-27 12:56:58 +0300

    Compound Assignment With Augmented Addition

--- freecodecamp/js/index.js
+++ freecodecamp/js/index.js
@@ -1,5 +1,8 @@
-const product = 2.0 * 2.5;
+let a = 3;
+let b = 17;
+let c = 12;
 
-const quotient = 4.4 / 2.0; // Change this line
-
-const remainder = 11 % 3;
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
