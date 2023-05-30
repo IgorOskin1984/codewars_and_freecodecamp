39f7a778826582ebbf5e32748c17f956b3596e1d
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-30 09:24:00 +0300

    Iterate with JavaScript Do...While Loops

--- freecodecamp/js/index.js
+++ freecodecamp/js/index.js
@@ -1,14 +1,9 @@
-function multiplyAll(arr) {
-	let product = 1;
-	// Only change code below this line
-	for (let i = 0; i < arr.length; i++) {
-		for (let j = 0; j < arr[i].length; j++) {
-			product *= arr[i][j]
-		}
-	}
+// Setup
+const myArray = [];
+let i = 10;
 
-	// Only change code above this line
-	return product;
-}
-
-multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+// Only change code below this line
+do {
+	myArray.push(i);
+	i++;
+} while (i < 10)
