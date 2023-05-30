1680af08353ccdc85247e568ca846711033ab08b
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-30 09:59:11 +0300

    Replace Loops using Recursion

--- freecodecamp/js/index.js
+++ freecodecamp/js/index.js
@@ -1,9 +1,13 @@
-// Setup
-const myArray = [];
-let i = 10;
+function sum(arr, n) {
+	if (n === 0) {
+		return 0;
+	}
+	let a = sum(arr, n - 1) + arr[n - 1];
+	return a
+}
+// Пример использования:
+const arr = [11, 22, 33, 44, 5];
+const n = 3;
 
-// Only change code below this line
-do {
-	myArray.push(i);
-	i++;
-} while (i < 10)
+const result = sum(arr, n);
+console.log(result); // Вывод: 6 (1 + 2 + 3)
