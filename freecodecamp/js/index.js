8ccb367adcee478fd4dbbb1677a8ad35d13bcec8
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-30 11:01:09 +0300

    Profile Lookup

--- freecodecamp/js/index.js
+++ freecodecamp/js/index.js
@@ -1,13 +1,46 @@
-function sum(arr, n) {
-	if (n === 0) {
-		return 0;
-	}
-	let a = sum(arr, n - 1) + arr[n - 1];
-	return a
-}
-// Пример использования:
-const arr = [11, 22, 33, 44, 5];
-const n = 3;
+// Setup
+const contacts = [
+	{
+		firstName: "Akira",
+		lastName: "Laine",
+		number: "0543236543",
+		likes: ["Pizza", "Coding", "Brownie Points"],
+	},
+	{
+		firstName: "Harry",
+		lastName: "Potter",
+		number: "0994372684",
+		likes: ["Hogwarts", "Magic", "Hagrid"],
+	},
+	{
+		firstName: "Sherlock",
+		lastName: "Holmes",
+		number: "0487345643",
+		likes: ["Intriguing Cases", "Violin"],
+	},
+	{
+		firstName: "Kristian",
+		lastName: "Vos",
+		number: "unknown",
+		likes: ["JavaScript", "Gaming", "Foxes"],
+	},
+];
 
-const result = sum(arr, n);
-console.log(result); // Вывод: 6 (1 + 2 + 3)
+function lookUpProfile(name, prop) {
+	// Only change code below this line
+	for (let i = 0; i < contacts.length; i++) {
+		if (contacts[i].firstName === name) {
+			if (contacts[i].hasOwnProperty(prop)) {
+				console.log(contacts[i][prop]);
+				return contacts[i][prop];
+			}
+			else {
+				return 'No such property';
+			}
+		}
+	}
+	return 'No such contact'
+	// Only change code above this line
+}
+
+lookUpProfile("Kristian", "lastName")
