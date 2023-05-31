723a56428b79c08f1893ec968babe0d2a60b331d
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-31 18:26:16 +0300

    Object.keys includes  Math.floor Math.min

--- codewars/index.js
+++ codewars/index.js
@@ -1,19 +1,35 @@
-function digitalRoot(num) {
-	console.log(num.toString().length);
+function cakes(recipe, available) {
+	//обозначить массив с переменными recipe и available
+	//сравнить массивы
+	//если в available нет нужного ингредиента - вернуть ложь
+	//пройтись по объекту available и значение каждого ингридиента делить на соответствующее значение recipe
+	//сохранять результаты деления в массиве сум
+	//вернуть наименьшее значение из этого массива
+	const recipeArr = Object.keys(recipe);
+	const availableArr = Object.keys(available);
+	const res = []
+	let isValid = true;
 
-	let sum = 0;
+	recipeArr.forEach((ingr) => {
+		isValid = availableArr.includes(ingr);
+		if (!isValid) return isValid
+	})
 
-	const numArr = num.toString().split('');
-	console.log(numArr);
-	for (let i = 0; i < numArr.length; i++) {
-		sum += parseInt(numArr[i]);
+	if (isValid === false) {
+		console.log(isValid);
+		return isValid;
 	}
-	console.log(sum);
+	else {
+		availableArr.forEach(ingr => {
+			let a = Math.floor(available[ingr] / recipe[ingr]);
+			isNaN(a)
+				? false
+				: res.push(a)
+		})
 
-	if (sum > 9) {
-		return digitalRoot(sum);
+		console.log(Math.min(...res))
+		return Math.min(...res)
 	}
-	return sum;
 }
 
-digitalRoot(389098)
+cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 });
