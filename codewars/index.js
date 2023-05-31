785ed6855856ed3d49d1f2134e04229aa76c9b36
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-05-31 11:23:11 +0300

    disemvowel

--- codewars/index.js
+++ codewars/index.js
@@ -1,11 +1,42 @@
-function getMiddle(s) {
-	let midle = Math.round(s.length / 2);
-	if (s.length % 2 == 0) {
-		return s[midle - 1] + s[midle];
+const disemvowel = (str) => {
+	console.log(str.length);
+	if (typeof str !== 'string') {
+		console.log('not a string');
+		return
+	} else {
+		//debugger
+		const vowels = ['a', 'e', 'i', 'o', 'u'];
+		let newStr = str.toLowerCase()
+		console.log(newStr);
+		console.log(newStr.length);
+		for (let i = 0; i < newStr.length; i++) {
+			console.log(newStr[i]);
+			if (vowels.includes(newStr[i])) {
+				str = str.replace(str[i], '');
+				newStr = newStr.replace(newStr[i], '');
+			}
+		}
+		console.log(newStr);
+		console.log(str);
 	}
-	else {
-		return s[midle - 1];
-	}
+	return str
 }
 
-getMiddle('testing')
+disemvowel2("No offense but,\nYour writing is among the worst I've ever read")
+
+function disemvowel2(str) {
+	console.log(str);
+	if (typeof str !== 'string') {
+		console.log('not a string');
+		return
+	} else {
+		const vowels = ['a', 'e', 'i', 'o', 'u'];
+		let newStr = '';
+		for (let i = 0; i < str.length; i++) {
+			if (!vowels.includes(str[i].toLowerCase())) {
+				newStr += (str[i]);
+			}
+		}
+		return newStr;
+	}
+}
