dce76479eb614ca6f02d009ad4507886d7cbe38f
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-01 18:01:13 +0300

    Create a Stateless Functional Component

--- freecodecamp/frontEnd/React/react.jsx
+++ freecodecamp/frontEnd/React/react.jsx
@@ -1,5 +1,9 @@
-const JSX = <div>
-	<h2>Welcome to React!</h2> <br />
-	<p>Be sure to close all tags!</p>
-	<hr />
-</div>
+const MyComponent = function () {
+	// Change code below this line
+	return <div>
+		<h2>Welcome to React!</h2> <br />
+		<p>Be sure to close all tags!</p>
+		<hr />
+	</div>
+	// Change code above this line
+}
