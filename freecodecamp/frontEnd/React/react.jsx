81c17d6d5d27ba85e8fbdc681c060a2643a9f2e3
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-01 19:29:48 +0300

    Render a Class Component to the DOM

--- freecodecamp/frontEnd/React/react.jsx
+++ freecodecamp/frontEnd/React/react.jsx
@@ -1,20 +1,3 @@
-class Fruits extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div>
-				<h2>Fruits:</h2>
-				{ /* Change code below this line */}
-				<NonCitrus />
-				<Citrus />
-				{ /* Change code above this line */}
-			</div>
-		);
-	}
-};
-
 class TypesOfFood extends React.Component {
 	constructor(props) {
 		super(props);
@@ -23,11 +6,15 @@ class TypesOfFood extends React.Component {
 		return (
 			<div>
 				<h1>Types of Food:</h1>
-				{ /* Change code below this line */}
+				{/* Change code below this line */}
 				<Fruits />
-				{ /* Change code above this line */}
 				<Vegetables />
+
+				{/* Change code above this line */}
 			</div>
 		);
 	}
 };
+
+// Change code below this line
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
