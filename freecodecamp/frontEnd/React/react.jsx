86948c5641a5e4bb9f7363cba0aa354e574867eb
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-05 16:39:27 +0300

    Create a Controlled Form

--- freecodecamp/frontEnd/React/react.jsx
+++ freecodecamp/frontEnd/React/react.jsx
@@ -1,29 +1,39 @@
-class ControlledInput extends React.Component {
+class MyForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			input: ''
+			input: '',
+			submit: ''
 		};
-		// Change code below this line
 		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(event) {
+		this.setState({
+			input: event.target.value
+		});
+	}
+	handleSubmit(event) {
+		event.preventDefault()
+		// Change code below this line
+		this.setState(state => ({
+			submit: state.input
+		}))
 		// Change code above this line
 	}
-	// Change code below this line
-	handleChange(event) {
-		this.setState(() => ({
-			input: event.target.value
-		}))
-	}
-	// Change code above this line
 	render() {
 		return (
 			<div>
-				{ /* Change code below this line */}
-				<input onChange={event => this.handleChange(event)} value={this.state.input} />
-				{ /* Change code above this line */}
-				<h4>Controlled Input:</h4>
-				<p>{this.state.input}</p>
+				<form onSubmit={this.handleSubmit}>
+					{/* Change code below this line */}
+					<input onChange={this.handleChange} value={this.state.input} />
+					{/* Change code above this line */}
+					<button onClick={this.handleSubmit} type='submit'>Submit!</button>
+				</form>
+				{/* Change code below this line */}
+				<h1>{this.state.submit || ''}</h1>
+				{/* Change code above this line */}
 			</div>
 		);
 	}
-};
+}
