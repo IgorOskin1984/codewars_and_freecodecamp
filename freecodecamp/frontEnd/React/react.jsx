dcaa8f01befbac1e5f9f166e84328271e2727303
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-05 18:08:51 +0300

    Optimize Re-Renders with shouldComponentUpdate

--- freecodecamp/frontEnd/React/react.jsx
+++ freecodecamp/frontEnd/React/react.jsx
@@ -1,37 +1,42 @@
-class MyComponent extends React.Component {
+class OnlyEvens extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('Should I update?');
+		// Change code below this line
+		if (nextProps.value % 2 !== 0) return false
+		else {
+			return this.props.value = nextProps.value
+		}
+		// Change code above this line
+	}
+	componentDidUpdate() {
+		console.log('Component re-rendered.');
+	}
+	render() {
+		return <h1>{this.props.value}</h1>;
+	}
+}
+
+class Controller extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			message: ''
+			value: 0
 		};
-		this.handleEnter = this.handleEnter.bind(this);
-		this.handleKeyPress = this.handleKeyPress.bind(this);
+		this.addValue = this.addValue.bind(this);
 	}
-
-	componentDidMount() {
-		document.addEventListener('keydown', this.handleKeyPress);
-	}
-
-	componentWillUnmount() {
-		document.removeEventListener('keydown', this.handleKeyPress);
-	}
-
-	handleEnter() {
-		this.setState((state) => ({
-			message: state.message + 'You pressed the enter key! '
+	addValue() {
+		this.setState(state => ({
+			value: state.value + 1
 		}));
 	}
-
-	handleKeyPress(event) {
-		if (event.keyCode === 13) {
-			this.handleEnter();
-		}
-	}
-
 	render() {
 		return (
 			<div>
-				<h1>{this.state.message}</h1>
+				<button onClick={this.addValue}>Add</button>
+				<OnlyEvens value={this.state.value} />
 			</div>
 		);
 	}
