7dd81f127b30706a522a2cdb99a1e08eb56922b6
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-06 17:44:29 +0300

    A Chain adding function ничего не понятно да и вроде не работает, хотя тест прошел

--- codewars/index.js
+++ codewars/index.js
@@ -1,42 +1,13 @@
-function generateHashtag(str) {
-	if (!str.trim().length) {
-		console.log('false');
-		return false
-	}
-	const arr = str.split(' ');
-	console.log(arr);
-	let result = arr.map(word => {
-		if (!word.trim().length) {
-			//console.log('trim().length');
-			return
-		}
-		let wordArr = word.split('');
-		wordArr[0] = wordArr[0].toUpperCase();
-		return wordArr.join('');
-	})
+function add(num) {
+	const fn = function (x) {
+		return add(num + x);
+	};
 
-	result = `#${result.join('')}`
-	console.log(result);
-	return (result.length < 141) ? result : false;
+	fn.valueOf = function () {
+		return num;
+	};
 
-}
-generateHashtag("code" + " ".repeat(140) + "wars")
-
-//*chat=========================================
-
-function generateHashtag(str) {
-	const words = str.trim().split(' ');
-
-	if (words.length === 0) {
-		return false;
-	}
-
-	const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
-
-	const hashtag = '#' + capitalizedWords.join('');
-
-	return (hashtag.length <= 140) ? hashtag : false;
+	return fn;
 }
 
-// Example usage:
-console.log(generateHashtag('hello world'));  // Output: "#HelloWorld"
+console.log(add(1)(2));
