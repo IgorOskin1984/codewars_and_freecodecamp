c9ba5db0c5007cd49a4191f778c261e73ee1df42
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-06 12:47:02 +0300

    generateHashtag

--- codewars/index.js
+++ codewars/index.js
@@ -1,12 +1,42 @@
-function moveZeros(arr) {
-	arr.forEach(num => {
-		if (num === 0) {
-			arr.splice(arr.indexOf(num), 1)
-			arr.push(num);
+function generateHashtag(str) {
+	if (!str.trim().length) {
+		console.log('false');
+		return false
+	}
+	const arr = str.split(' ');
+	console.log(arr);
+	let result = arr.map(word => {
+		if (!word.trim().length) {
+			//console.log('trim().length');
+			return
 		}
-	});
-	//console.log(arr);
-	return arr
+		let wordArr = word.split('');
+		wordArr[0] = wordArr[0].toUpperCase();
+		return wordArr.join('');
+	})
+
+	result = `#${result.join('')}`
+	console.log(result);
+	return (result.length < 141) ? result : false;
+
+}
+generateHashtag("code" + " ".repeat(140) + "wars")
+
+//*chat=========================================
+
+function generateHashtag(str) {
+	const words = str.trim().split(' ');
+
+	if (words.length === 0) {
+		return false;
+	}
+
+	const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+
+	const hashtag = '#' + capitalizedWords.join('');
+
+	return (hashtag.length <= 140) ? hashtag : false;
 }
 
-moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]);
+// Example usage:
+console.log(generateHashtag('hello world'));  // Output: "#HelloWorld"
