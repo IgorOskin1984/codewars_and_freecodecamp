3935b73a73a678d6c59fadb5bb8a3f35e15110d2
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-07 16:31:15 +0300

    Words to Hex

--- codewars/index.js
+++ codewars/index.js
@@ -1,15 +1,45 @@
-function find(array, element) {
-	return array.includes(element)
-		? array.indexOf(element)
-		: "Not found"
+//function wordsToHex(words) {
+//	const arr = words.trim().split(' ');
+//	const filteredArr = arr.filter((item) => item.length > 0);
+//	console.log(filteredArr);
+
+//	const hexArray = filteredArr.map((word) => {
+//		let hexNumber = '#';
+//		for (let i = 0; i < 3; i++) {
+//			hexNumber += word[i]
+//				? word[i].charCodeAt(0).toString(16)
+//				: '00';
+//		}
+//		return hexNumber;
+//	});
+
+//	return hexArray;
+//}
+
+
+
+
+
+function wordsToHex(string) {
+	const words = string.split(' ');
+
+	const hexValues = words.map((word) => {
+		let hex = '#';
+
+		// Take the first three letters of the word
+		const slicedWord = word.slice(0, 3);
+
+		// Convert each character to its ASCII code and append it to the hex value
+		for (let i = 0; i < 3; i++) {
+			const char = slicedWord[i];
+			const asciiCode = char ? char.charCodeAt(0).toString(16) : '00';
+			hex += asciiCode;
+		}
+
+		return hex;
+	});
+
+	return hexValues;
 }
 
-console.log(find([2, 3, 5, 7, 11], 5));
-
-
-//function find(array, element) {
-//  for (let i = 0; i < array.length; i++) {
-//    if (array[i] === element) return i;
-//  }
-//  return "Not found";
-//}
+console.log(wordsToHex('    Hello, my     name is Gary and I like cheese.'));
