ba9b2bbd80a8f0d650b0c65c34eba17cfc4ef0f6
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-08 10:33:39 +0300

    firstNonConsecutive

--- codewars/index.js
+++ codewars/index.js
@@ -1,7 +1,9 @@
-var number = function (array) {
-	return array.length
-		? array.map((el, ind) => `${ind + 1}: ${el}`)
-		: array
+function firstNonConsecutive(arr) {
+	if (!arr.length) return null
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i] - arr[i - 1] !== 1) return arr[i]
+	}
+	return null;
 }
 
-console.log(number([]));
+console.log(firstNonConsecutive([-7, -6, -4, -3, -2, 0, 1, 2]));
